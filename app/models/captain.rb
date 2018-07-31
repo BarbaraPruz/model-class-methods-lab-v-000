bd8d25b1fe9c5493647c5198c1f9fd2a8810e6eb
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-07-31 05:08:15 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -9,28 +9,35 @@ class Captain < ActiveRecord::Base
 
   def self.sailors
 #    find_captains_by_boat_class("Sailboat")
-    includes(boats: :classifications).where(classifications: { name: 'Sailboat' })
+    includes(boats: :classifications).where(classifications: { name: 'Sailboat' }).uniq
   end
-
+  
+  def self.motorboat_operators
+    includes(boats: :classifications).where(classifications: {name: "Motorboat"})
+  end
+  
   def self.talented_seafarers
-    motor_captains = find_captains_by_boat_class("Motorboat")
-    list = motor_captains & self.sailors
-    Captain.where(id: list.map(&:id))
+    # motor_captains = find_captains_by_boat_class("Motorboat")
+    # list = motor_captains & self.sailors
+    # Captain.where(id: list.map(&:id))
+    where("id IN (?)", self.sailors.pluck(:id) & self.motorboat_operators.pluck(:id))
   end
 
   def self.non_sailors
     # inefficient implementation - really want SQL query to pull all the captains that
     # don't have any sailboats...
-    list = self.all - self.sailors
-    Captain.where(id: list.map(&:id))
+    # list = self.all - self.sailors
+    # Captain.where(id: list.map(&:id))
+    where.not("id IN (?)", self.sailors.pluck(:id))
   end
 
-  def self.find_captains_by_boat_class (classification_name)
-    # convoluted logic - not sure how to get Classifactions to return ActiveRecord:relation
-    search = Classification.find_by(:name => classification_name)
-    boats = BoatClassification.find_boats_by_classification (search.id)
-    captains = boats.map { | boat | boat.captain }.uniq.compact
-    Captain.where(id: captains.map(&:id))
+  # Only needed for convoluted implementations commented out above
+  # def self.find_captains_by_boat_class (classification_name)
+  #   # convoluted logic - not sure how to get Classifactions to return ActiveRecord:relation
+  #   search = Classification.find_by(:name => classification_name)
+  #   boats = BoatClassification.find_boats_by_classification (search.id)
+  #   captains = boats.map { | boat | boat.captain }.uniq.compact
+  #   Captain.where(id: captains.map(&:id))
   end
 
 
